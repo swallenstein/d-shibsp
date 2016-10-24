eebb583a213d67f073eeb43f60130d694613e83b
r2h2 <rainer@hoerbe.at>
2016-10-24 20:24:59 +0200

    make sure to remove pid/lock files fordocker stop-start and autorestart

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -7,10 +7,18 @@ if [ $(id -u) -ne 0 ]; then
     exit 1
 fi
 
+
+# Make sure we're not confused by old, incompletely-shutdown shibd or httpd
+# context after restarting the container. httpd and shibd won't start correctly
+# if thinking it is already running.
+SHIBDLOCKFILE=/var/lock/subsys/shibd
+HTTPDPIDFILE=/var/run/httpd.pid
+rm -f $SHIBDLOCKFILE $HTTPDPIDFILE
+
 #SHIBSP_PREFIX=/opt/etc/shibboleth
 SHIBSP_PREFIX=/etc/shibboleth
 $SHIBSP_PREFIX/shibd-redhat start
 
-# Apache dislikes pre-existing PID files
-rm -f /var/logs/httpd.pid
 httpd -DFOREGROUND -d /opt/etc/httpd/ -f conf/httpd.conf
+
+exit 0
