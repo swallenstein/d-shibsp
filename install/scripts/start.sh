4d304e77e5cbc2f79077865250ecce83b97c7c5c
r2h2 <rainer@hoerbe.at>
2017-02-12 20:40:43 +0100

    fix start.sh

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -24,4 +24,4 @@ cleanup_and_prep
 
 /usr/sbin/shibd -u shibd05 -g root -p /var/run/shibboleth/shib.pid > /var/log/shibboleth/shibd_startup.log 2>&1
 
-httpd -DFOREGROUND -d /etc/httpd/ -f conf/httpd.conf > /var/log/httpd/httpd_startup.log 2&>1
+httpd -DFOREGROUND -d /etc/httpd/ -f conf/httpd.conf > /var/log/httpd/httpd_startup.log 2>&1
