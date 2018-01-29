d653e8131c2f27cf9ffef7ceb49e88270ef89ba9
rhoerbe <rainer@hoerbe.at>
2018-01-29 16:39:57 +0100

    write shibd + httpd startup messages to stdout/stderr instead startup.log

--- install/scripts/start.sh
+++ install/scripts/start.sh
@@ -27,7 +27,7 @@ function cleanup_and_prep {
 function start_shibd {
     echo "starting shibd" > /var/log/startup/start.log 2>&1
     export LD_LIBRARY_PATH=/opt/shibboleth/lib64
-    /usr/sbin/shibd -u $SHIBDUSER -g root -p /var/run/shibboleth/shib.pid >> /var/log/startup/start.log 2>&1
+    /usr/sbin/shibd -u $SHIBDUSER -g root -p /var/run/shibboleth/shib.pid
 }
 
 
@@ -35,9 +35,10 @@ function start_httpd {
     echo "starting httpd" >> /var/log/startup/start.log 2>&1
     # `docker run` 1.12.6 will reset ownership and permissions on /run/httpd; therefore it need to be done again:
     # do not start with root to avoid permission conflicts on log files
-    su - $HTTPDUSER  -c 'rm -f /run/httpd/*' >> /var/log/startup/start.log 2>&1
-    su - $HTTPDUSER  -c 'httpd -DFOREGROUND -d /etc/httpd/ -f conf/httpd.conf'  >> /var/log/startup/start.log 2>&1
-    su - $HTTPDUSER  -c 'echo "httpd started with pid=$(cat /run/httpd/httpd.pid)"' >> /var/log/startup/start.log 2>&1
+    su - $HTTPDUSER  -c 'rm -f /run/httpd/* 2>/dev/null || true'
+    su - $HTTPDUSER  -c 'httpd -t -d /etc/httpd/ -f conf/httpd.conf'
+    su - $HTTPDUSER  -c 'httpd -DFOREGROUND -d /etc/httpd/ -f conf/httpd.conf'
+    su - $HTTPDUSER  -c 'echo "httpd started with pid=$(cat /run/httpd/httpd.pid)"'
 }
 
 
